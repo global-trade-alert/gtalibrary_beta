f232aa04f692f6aad4d196c7226b96e0f42f2ffe
svensglinz <svenglinz@gmail.com>
2023-04-19 15:15:01 +0200

    changes

--- R/test function.r
+++ R/test function.r
@@ -3,7 +3,7 @@ test_function <- function(tbl) {
     # tbl <- data.table::data.table(a = c(1:10), b = c(10:19))
 
     filter_statement <- parse(text = "a %in% c(1, 5, 8) | b == 15")
-    out <- tbl[eval(filter_statement)]
+    out <- with(tbl, tbl[eval(filter_statement)])
 
     return(out)
 }
