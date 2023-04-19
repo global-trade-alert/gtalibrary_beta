12b283a78e0c854d0375b92a2197d5119a90284a
svensglinz <svenglinz@gmail.com>
2023-04-19 08:58:18 +0200

    changes

--- R/test function.r
+++ R/test function.r
@@ -1,10 +1,10 @@
 #' @export
-test_function <- function() {
-    tbl <- tibble::tibble(a = c(1:10), b = c(10:19))
+test_function <- function(tbl) {
+    # tbl <- tibble::tibble(a = c(1:10), b = c(10:19))
 
-    filter_statement <- "a %in% c(1, 5, 8) | b == 15"
+    filter_statement <- ("a %in% c(1, 5, 8) | b == 15")
     out <- tbl |>
-        dplyr::filter(eval(parse(text = filter_statement)))
+        dplyr::filter(!!rlang::parse_expr(filter_statement))
 
     return(out)
 }
