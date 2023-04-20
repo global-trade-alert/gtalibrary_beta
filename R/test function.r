dc127d1156cfb032d6eb1d8e6055845ebbb216ae
svensglinz <svenglinz@gmail.com>
2023-04-20 18:35:58 +0200

    changes

--- R/test function.r
+++ R/test function.r
@@ -1,13 +1,12 @@
 #' @export
 test_function <- function(tbl) {
-    # tbl <- data.table::data.table(a = c(1:10), b = c(10:19))
+    tbl <- tibble::tibble(a = c(1:10), b = c(10:19))
 
     filter_statement <- substitute(a %in% c(1, 5, 8))
 
     # append to filter_statement (for lack of a better solution with dtplyr...)
-    filter_statement <- substitute(filter_statement | b == 15, list(filter_statement = filter_statement))
     out <- dtplyr::lazy_dt(tbl) |>
-        dplyr::filter(eval.parent(filter_statement))
+        dplyr::filter(eval(filter_statement, enclos = parent.frame()))
 
     return(out)
 }
