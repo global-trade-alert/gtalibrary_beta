313ac4a5ae5f19d04fa5f4b5a4dd567e1f4e6372
svensglinz <svenglinz@gmail.com>
2023-04-21 08:49:20 +0200

    changes

--- R/test function.r
+++ R/test function.r
@@ -11,3 +11,14 @@ test_function <- function(tbl) {
 
     return(out)
 }
+
+test_function_2 <- function(tbl) {
+    tbl <- tibble::tibble(a = c(1:10), b = c(10:19))
+
+    # append to filter_statement (for lack of a better solution with dtplyr...)
+    data <- dtplyr::lazy_dt(tbl)
+    out <- data |>
+        dplyr::filter(a %in% c(1, 5, 8))
+
+    return(out)
+}
